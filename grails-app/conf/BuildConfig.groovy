7b9f1c8703c383746dfff514506483554f22dae4
Markus List <mlist@health.sdu.dk>
2014-05-30 10:39:07 +0200

    include backend as dependency again

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -27,7 +27,7 @@
  *
  * ############################################################################
  */
-//grails.plugin.location.backend = "../OpenLabBackend"
+grails.plugin.location.backend = "../OpenLabBackend"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
